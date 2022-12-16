5f67dc1291832ff9b27d213228a5f70de2722098
unknown <victordalet@protonmail.com>
2022-12-16 15:58:07 +0100

    responsive

--- src/components/logo.js
+++ src/components/logo.js
@@ -2,9 +2,9 @@ import React from 'react';
 
 const Logo = () => {
     return (
-        <div className="logo">
-            <h3>Template</h3>
-        </div>
+            <div className="logo">
+                <h3>Template</h3>
+            </div>
     );
 };
 
